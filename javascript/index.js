71a902f7407444170c7fffa4aaa519a68c69cbd8
Roger B <myvarequals@gmail.com>
2018-05-30 03:26:55 -0400

    landing page

--- javascript/index.js
+++ javascript/index.js
@@ -2,17 +2,19 @@ $(document).ready(function () {
 
   $('#sidebar-close, #overlay').on('click', function () {
     document.getElementById('nav-sidebar').style.width = '0px';
-    document.getElementById('maincontent').style.marginTop = '1em';
     document.getElementById('overlay').style.display = 'none';
+    document.getElementById('maincontent').style.marginLeft = '0px';
+    document.getElementById('maincontent').style.marginTop = '0';
+
       $('#headcontent').fadeIn();
 
   });
 
   $('#burger').on('click', function () {
+    $('#headcontent').fadeOut();
     document.getElementById('nav-sidebar').style.width = '150px';
     document.getElementById('maincontent').style.marginLeft = '150px';
-    document.getElementById('maincontent').style.marginTop = '45px';
+    document.getElementById('maincontent').style.marginTop = '55px';
     document.getElementById('overlay').style.display = 'inherit';
-    $('#headcontent').fadeOut();
   });
 });
