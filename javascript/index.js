90416939f3ea16ab4f45203469622e35e94dabb3
Roger B <myvarequals@gmail.com>
2018-06-01 03:11:39 -0400

    smoothed sidenav, added blank htmls

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,28 @@
 $(document).ready(function () {
 
+  $(window).resize(function() {
+    if ($(window).width() >= 768) {
+      $('main').addClass('mx-auto')
+    } else {
+        $('main').removeClass('mx-auto')
+    }
+  })
+
   $('#sidebar-close, #overlay').on('click', function () {
     document.getElementById('nav-sidebar').style.width = '0px';
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('maincontent').style.marginLeft = '0px';
-    document.getElementById('maincontent').style.marginTop = '0';
+  //  document.getElementById('maincontent').style.marginTop = '0';
 
-      $('#headcontent').fadeIn();
+      $('#side-logo').fadeIn();
 
   });
 
   $('#burger').on('click', function () {
-    $('#headcontent').fadeOut();
+    $('#side-logo').fadeOut();
     document.getElementById('nav-sidebar').style.width = '150px';
     document.getElementById('maincontent').style.marginLeft = '150px';
-    document.getElementById('maincontent').style.marginTop = '55px';
+  //  document.getElementById('maincontent').style.marginTop = '55px';
     document.getElementById('overlay').style.display = 'inherit';
   });
 });
