41c680fce1a69df9acd30fcf53f5ec964c531c46
Roger B <myvarequals@gmail.com>
2018-06-02 20:02:38 -0400

    working on about layout

--- javascript/index.js
+++ javascript/index.js
@@ -13,22 +13,19 @@ $(document).ready(function () {
         $('main').removeClass('mx-auto')
     }
   })
-
+  //Close Sidenav
   $('#sidebar-close, #overlay').on('click', function () {
     document.getElementById('nav-sidebar').style.width = '0px';
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('maincontent').style.marginLeft = '0px';
-  //  document.getElementById('maincontent').style.marginTop = '0';
-
-      $('#side-logo').fadeIn();
+    $('#side-logo').fadeIn();
 
   });
-
+  // Open Sidenav
   $('#burger').on('click', function () {
     $('#side-logo').fadeOut();
     document.getElementById('nav-sidebar').style.width = '150px';
     document.getElementById('maincontent').style.marginLeft = '150px';
-  //  document.getElementById('maincontent').style.marginTop = '55px';
     document.getElementById('overlay').style.display = 'inherit';
   });
 });
