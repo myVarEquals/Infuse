c3f3c5d5140c1dc541108ab2b91911fbcd853656
Roger B <myvarequals@gmail.com>
2018-06-15 01:53:20 -0400

    added some js to info, more content to products

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
   $(function() {
     if ($(window).width() >= 768) {
       $('main').addClass('mx-auto')
@@ -28,4 +32,43 @@ $(document).ready(function () {
     document.getElementById('maincontent').style.marginLeft = '150px';
     document.getElementById('overlay').style.display = 'inherit';
   });
+
+  $(function() {
+
+    $('.step-one .steplists .list').each(function(i) {
+
+      setTimeout(function() {
+        $('.step-one .steplists .list').eq(i).addClass('listappear')
+      }, 150 * (i+1))
+    })
+
+  })
+
+  $(window).scroll(function() {
+
+    var scrolling = $(this).scrollTop()
+
+    if (scrolling > $('.step-three').offset().top - 500) {
+      
+      $('.step-three .steplists .list').each(function(i) {
+
+        setTimeout(function() {
+          $('.step-three .steplists .list').eq(i).addClass('listappear')
+        }, 150 * (i+1))
+      })
+
+    }
+
+    if (scrolling > $('.step-two').offset().top - 500) {
+
+      $('.step-two .steplists .list').each(function(i) {
+
+        setTimeout(function() {
+          $('.step-two .steplists .list').eq(i).addClass('listappear')
+        }, 150 * (i+1))
+      })
+
+    }
+
+  })
 });
